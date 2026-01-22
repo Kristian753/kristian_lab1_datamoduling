579ca63d025dc1eadf96b69418c1fb9671b389de
Kristian Tawfeeq <kristiantawfeeq@MacBook-Pro-som-tillhor-Kristian.local>
2026-01-22 13:21:51 +0100

    påbörjat insert data

--- yh_lab/create_table.sql
+++ yh_lab/create_table.sql
@@ -1,8 +1,4 @@
-CREATE TABLE program (
-    person_id SERIAL PRIMARY KEY,
-    namn TEXT
-);
-
+-- Tabell för känsliga personuppgifter lagras
 CREATE TABLE person (
     person_id SERIAL PRIMARY KEY,
     fornamn TEXT,
@@ -10,36 +6,41 @@ CREATE TABLE person (
     personnummer TEXT,
     email TEXT
 );
-
+-- Tabell för utbildningsprogram inom skolan
+CREATE TABLE program (
+    program_id SERIAL PRIMARY KEY,
+    namn TEXT
+);
+-- Tabell för studenter kopplade personer
 CREATE TABLE student (
     student_id SERIAL PRIMARY KEY,
     person_id INT REFERENCES person(person_id),
     klass_id INT REFERENCES klass(klass_id)
 );
-
+-- Tabell för klasser inom program
 CREATE TABLE klass (
     klass_id SERIAL PRIMARY KEY,
     program_id INT REFERENCES program(program_id),
     utbildningsledare_id INT REFERENCES utbildningsledare(utbildningsledare_id)
 );
-
+-- Tabell för utbildningsledare kopplade personer
 CREATE TABLE utbildningsledare (
     utbildningsledare_id SERIAL PRIMARY KEY,
     person_id INT REFERENCES person(person_id)
 );
-
+-- Tabell för kurser inom utbildning
 CREATE TABLE kurs (
     kurs_id SERIAL PRIMARY KEY,
     namn TEXT,
     kurskod TEXT,
     poang INT
 );
-
+-- Tabell för utbildare kopplade personer
 CREATE TABLE utbildare (
     utbildare_id SERIAL PRIMARY KEY,
     person_id INT REFERENCES person(person_id)
 );
-
+-- Kopplingstabell mellan utbildare och kurser
 CREATE TABLE utbildare_kurs (
     utbildare_id INT REFERENCES utbildare(utbildare_id),
     kurs_id INT REFERENCES kurs(kurs_id),
---
+++ yh_lab/insert_data.sql
@@ -0,0 +1,16 @@
+-- Lägger in personuppgifter för alla.
+INSERT INTO person (fornman, efternamn, personnummer, email) VALUES
+('Kristian', 'Tawfeeq', '20060723-6932', 'kristiantawfeeq33@gmail.com'),
+('Angham', 'Ziad', '19790523-1283', 'anghamziad@gmail.com'),
+('Alawie', 'Galbi', '19900101-1459', 'alawiegalbi@gmail.com');
+-- Skapar ett utbildningsprogram i databasen.
+INSERT INTO program (namn) VALUES
+('Data engineer');
+-- Kopplar utbildningsledare till person.
+INSERT INTO utbildningsledare (person_id) VALUES
+(1);
+-- Skapar klass kopplad till program.
+INSERT INTO klass (program_id, utbildningsledare_id) VALUES
+(1, 1);
+
+
