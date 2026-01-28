4cf262c7b5e6419c33303ba349ac69ff7c13ef1e
Kristian Tawfeeq <kristiantawfeeq@Mac.lan>
2026-01-28 02:17:59 +0100

    fortsättning med insert data

--- yh_lab/insert_data.sql
+++ yh_lab/insert_data.sql
@@ -1,48 +1,34 @@
--- Lägger in personuppgifter för alla.
-INSERT INTO person (fornamn, efternamn, personnummer, email) VALUES
-('Kristian', 'Tawfeeq', '20060723-6932', 'kristiantawfeeq33@gmail.com'),
-('Angham', 'Ziad', '19790523-1283', 'anghamziad@gmail.com'),
-('Alawie', 'Galbi', '19900101-1459', 'alawiegalbi@gmail.com'),
-('Abo', 'Sargon', '19531123-7854', 'abosargon@gmail.com'),
-('Jalal', 'Ouzo', '19641230-0479', 'jalalouzo@gmail.com');
--- Skapar ett utbildningsprogram i databasen.
-INSERT INTO program (program_id, program_namn) VALUES
-(1,'Data engineer'),
-(2,'Webb developer'),
-(3,'automation engineer');
--- Lägger in studenter kopplade till person och klass.
-INSERT INTO student (student_id, person_id, klass_id) VALUES
-(1, 1, 1),
-(2, 2, 2),
-(3, 3, 3),
-(4, 4, 1),
-(5, 5, 2);
--- Skapar klass kopplad till program.
-INSERT INTO klass (program_id, klass_namn, utbildningsledare_id) VALUES
-(1,'DE25', 1), -- Data Engineer år 1
-(2,'DE24', 2), -- Data Engineer år 2
-(3,'DE23', 3), -- Data Engineer år 3
--- webb developer klasser
-(2,'WD25', 1), -- Webb Developer år 1
-(2,'WD24', 2), -- Webb Developer år 2
-(2,'WD23', 3), -- Webb Developer år 3
--- automation engineer klasser
-(3,'AE25', 1), -- Automation Engineer år 1
-(3,'AE24', 2), -- Automation Engineer år 2
-(3,'AE23', 3); -- Automation Engineer år 3
+INSERT INTO program (program_namn) VALUES
+('Data Engineering'),
+('Webbutveckling'),
+('Nätverkstekniker');
 
+INSERT INTO utbildningsledare (fornamn, efternamn, email) VALUES
+('Angham', 'Ziad', 'anghamziad@gmail.com'),
+('Jalal', 'Ouzo', 'jalalouzo@gmail.com'),
+('Hules', 'Tawfeeq', 'hulestawfeeq@gmail.com'),
+('Abo', 'Sargon', 'abosargon@gmail.com');
+-- Data Engineering (program_id = 1) 3år
+INSERT INTO klass (klass_namn, program_id, ledare_id) VALUES
+('DE23', 1, 1), -- Data Engineering 2023
+('DE24', 1, 1), -- Data Engineering 2024
+('DE25', 1, 2); -- Data Engineering 2025
 
+-- Webbutveckling (program_id = 2) 3år
+('WB23', 2, 2), -- Webbutveckling 2023
+('WB24', 2, 3), -- Webbutveckling 2024
+('WB25', 2, 3); -- Webbutveckling 2025
 
--- Kopplar utbildningsledare till person.
-INSERT INTO utbildningsledare (person_id, fornamn, efternamn, email) VALUES
-(1,'Hazim', 'Yalda', 'hazimy@gmail.com'),
-(2,'Gorgis', 'Yalda', 'gorgisy@gmail.com'),
-(3,'Hules', 'Jager', 'hulesjager@gmail.com');
+-- Nätverkstekniker (program_id = 3) 3år
+('NT23', 3, 4), -- Nätverkstekniker 2023
+('NT24', 3, 4), -- Nätverkstekniker 2024
+('NT25', 3, 1); -- Nätverkstekniker 2025
 
-INSERT INTO kurs (kurs_namn, kurskod, poang, beskrivning, program_id) VALUES
-('Databasteknik', 'DE1010', 7.5, 'Introduktion till databasteknik och SQL.', 1),
-('Webbutveckling', 'WD2020', 7.5, 'Grundläggande webbutveckling med HTML, CSS och JavaScript.', 2),
-('Automationssystem', 'AE3030', 7.5, 'Principer för design och implementering av automationssystem.', 3);
+INSERT INTO student (fornamn, efternamn, personnummer, email, klass_id) VALUES
+-- DE25;
+('Gorgis', 'Yalda', '19900101-1234', 'gorgisyalda@gmail.com', 1),
+('Maya', 'Leon', '19920202-2345', 'mayaleon@gmail.com', 1)
 
-
-INSERT INTO utbildare (utbildare_id, person_id) VALUES
+-- DE24;
+('Marcilia', 'Gilana', '19930303-3456', 'marcilia.gilana@gmail.com', 2),
+('Paula', 'Somie', '19940404-4567', 'paulasomie@gmail.com', 2)
